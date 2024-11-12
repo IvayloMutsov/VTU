62330d69e668b2abdabdb2bada9e9fe5ed11cb46
IvayloMutsov <145490251+IvayloMutsov@users.noreply.github.com>
2024-11-12 21:36:40 +0200

    Add files via upload

---
+++ VTU_Homework/Homework_zad_2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Homework_zad_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of boxes: ");
+            int numverOfBoxes = int.Parse(Console.ReadLine());
+            if (numverOfBoxes > 40 || numverOfBoxes < 1)
+            {
+                Console.WriteLine("You cannot have more than 40 or less than 1 boxes");
+
+            }
+            else
+            {
+                int yellowApplesCount = 0;
+                int redApplesCount = 0;
+                int appleDifference;
+                for (int i = 1; i <= numverOfBoxes; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        redApplesCount += i * i;
+                    }
+                    else
+                    {
+                        yellowApplesCount += i * i;
+                    }
+                }
+                appleDifference = yellowApplesCount - redApplesCount;
+                Console.WriteLine($"The difference between yellow and red apples is {appleDifference}");
+            }
+        }
+    }
+}
---
+++ VTU_Homework/VTU_Homework/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace VTU_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zad 1
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("Enter the lenght of the train (meters): ");
+            double trainLenght = double.Parse(Console.ReadLine());
+            Console.Write("Enter the speed of the train (m/sec): ");
+            double trainSpeed = double.Parse(Console.ReadLine());
+            Console.Write("Entr the time it takes the tran to pass through the bridge or tunel (sec):");
+            double seconds = double.Parse(Console.ReadLine());
+            double totalDistance = trainSpeed * seconds;
+            double bridgeOrTunelDistance = totalDistance - trainLenght;
+            Console.WriteLine($"The length of the bridge is {bridgeOrTunelDistance} meters");
+        }
+    }
+}
