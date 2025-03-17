bc5a5734d4ec68abe126afa87adee1f7f8bf89a5
IvayloMutsov <IGMutsov@gmail.com>
2025-03-17 14:26:53 +0200

    .

--- ORM/zad1/Database.cs
+++ ORM/zad1/Database.cs
@@ -22,54 +22,61 @@ namespace ORM
             using (SqlConnection connection = new SqlConnection(tempConnection))
             {
                 connection.Open();
-                SqlCommand cmd = new SqlCommand($@"
+                SqlCommand cmd = new SqlCommand(@"
                 IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = @DatabaseName)
                 BEGIN
-                    CREATE DATABASE {databaseName};
-                END
-        ", connection);
+                    DECLARE @sql NVARCHAR(MAX);
+                    SET @sql = 'CREATE DATABASE [' + @DatabaseName + ']';
+                    EXEC sp_executesql @sql;
+                END", connection);
 
-                cmd.Parameters.AddWithValue("@DatabaseName", databaseName);
+                cmd.Parameters.Add(new SqlParameter("@DatabaseName", databaseName));
                 cmd.ExecuteNonQuery();
             }
         }
 
-
-        public void CreateTable(string tableName) // Creating table it it does not exist
+        public void CreateTable(string tableName) // Creating table if it does not exist
         {
             if (tableName != "x")
             {
-              string createTableQuery = $@"
-             IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = @TableName)
-             BEGIN
-                CREATE TABLE {tableName} (
-                {tableName}Id INT PRIMARY KEY IDENTITY(1,1),
-                {tableName}Name NVARCHAR(30) NOT NULL
-                );
-             END";
+                string createTableQuery = @"
+                IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = @TableName)
+                BEGIN
+                    DECLARE @sql NVARCHAR(MAX);
+                    SET @sql = 'CREATE TABLE [' + @TableName + '] (
+                        [' + @TableName + 'Id] INT PRIMARY KEY IDENTITY(1,1),
+                        [' + @TableName + 'Name] NVARCHAR(30) NOT NULL
+                    )';
+                    EXEC sp_executesql @sql;
+                END";
 
                 using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
                     connection.Open();
                     SqlCommand cmd = new SqlCommand(createTableQuery, connection);
-                    cmd.Parameters.AddWithValue("@TableName", tableName);
+                    cmd.Parameters.Add(new SqlParameter("@TableName", tableName));
                     cmd.ExecuteNonQuery();
                 }
             }
         }
 
-        public void InsertValuesIntoTable(string tableName, string[] tableValue) // Insearting values in the table
+        public void InsertValuesIntoTable(string tableName, string[] tableValue) // Inserting values in the table
         {
             if (tableName != "x")
             {
                 foreach (var item in tableValue)
                 {
-                    string insertQuerry = $@"INSERT INTO {tableName} ({tableName}Name) VALUES (@Value);";
+                    string insertQuery = @"
+                    DECLARE @sql NVARCHAR(MAX);
+                    SET @sql = 'INSERT INTO [' + @TableName + '] ([' + @TableName + 'Name]) VALUES (@Value)';
+                    EXEC sp_executesql @sql, N'@Value NVARCHAR(30)', @Value = @Value;";
+
                     using (SqlConnection connection = new SqlConnection(_connectionString))
                     {
                         connection.Open();
-                        SqlCommand command = new SqlCommand(insertQuerry, connection);
-                        command.Parameters.AddWithValue("@Value", item);
+                        SqlCommand command = new SqlCommand(insertQuery, connection);
+                        command.Parameters.Add(new SqlParameter("@TableName", tableName));
+                        command.Parameters.Add(new SqlParameter("@Value", item));
                         command.ExecuteNonQuery();
                     }
                 }
@@ -78,37 +85,50 @@ namespace ORM
 
         public void CreateRelation(string relationName, string table1, string table2) // Creating a many-to-many relation
         {
-            string relationQuerry = $@"
-            IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{relationName}')
+            string relationQuery = @"
+            IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = @RelationName)
             BEGIN
-                 CREATE TABLE {relationName} (
-                    {relationName}Id INT PRIMARY KEY IDENTITY(1,1),
-                    {table1}Id INT,
-                    {table2}Id INT,
-                    FOREIGN KEY ({table1}Id) REFERENCES {table1}({table1}Id),
-                    FOREIGN KEY ({table2}Id) REFERENCES {table2}({table2}Id)
-                    );
+                DECLARE @sql NVARCHAR(MAX);
+                SET @sql = 'CREATE TABLE [' + @RelationName + '] (
+                    [' + @RelationName + 'Id] INT PRIMARY KEY IDENTITY(1,1),
+                    [' + @Table1 + 'Id] INT,
+                    [' + @Table2 + 'Id] INT,
+                    FOREIGN KEY ([' + @Table1 + 'Id]) REFERENCES [' + @Table1 + ']([' + @Table1 + 'Id]),
+                    FOREIGN KEY ([' + @Table2 + 'Id]) REFERENCES [' + @Table2 + ']([' + @Table2 + 'Id])
+                )';
+                EXEC sp_executesql @sql;
             END";
 
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
-                SqlCommand cmd = new SqlCommand(relationQuerry,connection);
+                SqlCommand cmd = new SqlCommand(relationQuery, connection);
+                cmd.Parameters.Add(new SqlParameter("@RelationName", relationName));
+                cmd.Parameters.Add(new SqlParameter("@Table1", table1));
+                cmd.Parameters.Add(new SqlParameter("@Table2", table2));
                 connection.Open();
-                cmd.Parameters.AddWithValue("@TableName", relationName);
                 cmd.ExecuteNonQuery();
             }
         }
 
-        public void CustomerBuyProduct(int[] tableValue, string tableName) //inserting values in the relation table
+        public void CustomerBuyProduct(int[] tableValue, string tableName) // Inserting values in the relation table
         {
             List<string> cols = GetColumnNames(tableName);
             if (!tableValue.Contains(0))
             {
-                string orderQuerry = $@"INSERT INTO {tableName} ({cols[1]},{cols[2]}) VALUES ({tableValue[0]},{tableValue[1]});";
+                string orderQuery = @"
+                DECLARE @sql NVARCHAR(MAX);
+                SET @sql = 'INSERT INTO [' + @TableName + '] ([' + @Col1 + '], [' + @Col2 + ']) VALUES (@Value1, @Value2)';
+                EXEC sp_executesql @sql, N'@Value1 INT, @Value2 INT', @Value1 = @Value1, @Value2 = @Value2;";
+
                 using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
                     connection.Open();
-                    SqlCommand command = new SqlCommand(orderQuerry, connection);
+                    SqlCommand command = new SqlCommand(orderQuery, connection);
+                    command.Parameters.Add(new SqlParameter("@TableName", tableName));
+                    command.Parameters.Add(new SqlParameter("@Col1", cols[1]));
+                    command.Parameters.Add(new SqlParameter("@Col2", cols[2]));
+                    command.Parameters.Add(new SqlParameter("@Value1", tableValue[0]));
+                    command.Parameters.Add(new SqlParameter("@Value2", tableValue[1]));
                     command.ExecuteNonQuery();
                 }
             }
@@ -122,7 +142,7 @@ namespace ORM
                 string query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @TableName;";
 
                 SqlCommand cmd = new SqlCommand(query, connection);
-                cmd.Parameters.AddWithValue("@TableName", tableName);
+                cmd.Parameters.Add(new SqlParameter("@TableName", tableName));
 
                 connection.Open();
 
@@ -134,9 +154,7 @@ namespace ORM
                     }
                 }
             }
-
             return columns;
         }
-
     }
 }
