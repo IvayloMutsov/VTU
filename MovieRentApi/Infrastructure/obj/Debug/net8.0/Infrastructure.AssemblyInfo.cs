ae3ed1ae90deadb450cdebd98000fabbb9aeaf9a
IvayloMutsov <IGMutsov@gmail.com>
2025-10-08 00:39:40 +0300

    added migration

--- MovieRentApi/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ MovieRentApi/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c08947c5e944515bcd51d0537fe1be9d6179da18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5e57caa1ed2e5a4e8e48ee2cdec28340406303")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
