c2d718e99fac3e8ea328e4e6ecb6245ad6af80fb
IvayloMutsov <145490251+IvayloMutsov@users.noreply.github.com>
2026-02-17 22:31:12 +0200

    .

--- LibraryMVC/LibraryMVC/obj/Debug/net9.0/LibraryMVC.AssemblyInfo.cs
+++ LibraryMVC/LibraryMVC/obj/Debug/net9.0/LibraryMVC.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f04396f7add6843e645ffd94c615b91b6da9af56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b27e3aa3f909b5ebb09d184c2871f5cc6c7af95")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
