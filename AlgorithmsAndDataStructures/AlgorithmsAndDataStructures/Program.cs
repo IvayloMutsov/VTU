709039a89d1d223dc40859bedcde61be4509ab9f
IvayloMutsov <145490251+IvayloMutsov@users.noreply.github.com>
2026-02-21 15:00:27 +0200

    .

---
+++ AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace AlgorithmsAndDataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 30000;
+            int[] input = new int[n];
+            int[] input2 = new int[n];
+            Random rand = new Random();
+            Sorting.RandomData(input, rand);
+            Stopwatch w1 = new Stopwatch();
+            Stopwatch w2 = new Stopwatch();
+            w1.Start();
+            Sorting.BubbleSort(input);
+            w1.Stop();
+            w2.Start();
+            Sorting.SelectionSort(input2);
+            w2.Stop();
+            Console.WriteLine($"Bubble - {w1.ElapsedMilliseconds} / Selection - {w2.ElapsedMilliseconds}");
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/Sorting.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndDataStructures
+{
+    public class Sorting
+    {
+        public static void RandomData(int[] data, Random rand)
+        {
+            int len = data.Length;
+            for (int i = 0; i < len; i++)
+            {
+                data[i] = rand.Next(3*len);
+            }
+        }
+
+        public static void PrintData(int[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write(data[i] + " ");
+            }
+        }
+
+        public static void SelectionSort(int[] data)
+        {
+            for (int j = 0; j < data.Length - 1; j++)
+            {
+                int min = data[j] , pos = j;
+                for (int i = j + 1; i < data.Length; i++)
+                {
+                    if (data[i] < min)
+                    {
+                        min = data[i];
+                        pos = i;
+                    }
+                }
+                int temp = data[j];
+                data[j] = min;
+                data[pos] = temp;
+            }
+
+        }
+
+        public static void BubbleSort(int[] data)
+        {
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                for (int j = data.Length - 1; j > i ; j--)
+                {
+                    if (data[j - 1] > data[j])
+                    {
+                        int temp = data[j];
+                        data[j] = data[j - 1];
+                        data[j - 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
